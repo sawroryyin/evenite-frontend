f948cc8ab874276316fd1b4260cf91ed3903c881
saw_rory_yin <sawroryyinmky.26@gmail.com>
2026-05-12 17:34:09 +0700

    set up event create options

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EventCreateOptionsView from '../views/EventCreateOptionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
+    { path: '/', name: 'home', component: HomeView, },
+    { path: '/create', name: 'create-options', component: EventCreateOptionsView }
   ]
 })
 
